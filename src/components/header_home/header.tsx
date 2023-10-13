41a06b400afbf524006d376e2e2d6b0a6aed8582
Raianna Lima <raiannacodelima@gmail.com>
2023-10-13 12:25:11 -0300

    Feat: header_home init.

---
+++ src/components/header_home/header.tsx
@@ -0,0 +1,22 @@
+export const header_home = () => {
+    return (
+        <header>
+            <div className="header_home_logo">
+                <h1>
+                    Motors
+                </h1>
+                <span>
+                    shop
+                </span>
+            </div>
+            <div className="header_home_nav">
+                <button type="button">
+                    Fazer Login
+                </button>
+                <button type="button">
+                    Cadastrar
+                </button>
+            </div>
+        </header>
+    )
+}
