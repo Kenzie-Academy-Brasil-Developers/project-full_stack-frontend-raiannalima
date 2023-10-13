bb4f09825e62f8e983cf1f95a4de67e00a4dca3f
Raianna Lima <raiannacodelima@gmail.com>
2023-10-13 12:30:06 -0300

    Feat: header_profile init.

---
+++ src/components/header_profile/header_profile.tsx
@@ -0,0 +1,20 @@
+export const header_profile = () => {
+    return (
+        <header>
+            <div className="header_home_logo">
+                <h1>
+                    Motors
+                </h1>
+                <span>
+                    shop
+                </span>
+            </div>
+            <div className="header_home_nav">
+                <img src=""></img>
+                <span>
+                    Samuel Leão
+                </span>
+            </div>
+        </header>
+    )
+}
