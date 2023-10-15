b459652f6fac501476d198005315348e7454f937
Raianna Lima <raiannacodelima@gmail.com>
2023-10-15 16:53:38 -0300

    Feat: Update card comment.

--- src/components/card_comment/card_comment.tsx
+++ src/components/card_comment/card_comment.tsx
@@ -4,17 +4,17 @@ export const Card_comment = () => {
 
     return (
         <li>
-            <div className="">
-                <img src={avatar}>
+            <div className="flex mb-3">
+                <img src={avatar} className="w-[32px] h-[32px] mr-[0.5rem]">
                 </img>
-                <span>
+                <span className="mr-[0.5rem] text-grey1 text-sm font-medium">
                     Júlia Lima
                 </span>
-                <span>
+                <span className="text-grey3 font-normal text-xs">
                     •  há 3 dias
                 </span>
             </div>
-            <p>
+            <p className="text-grey2 text-sm font-normal">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book.
